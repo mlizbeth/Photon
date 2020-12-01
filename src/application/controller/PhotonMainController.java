5463a4960dab771f4752e8e32bd3bde7025c971f
Madeline <1906931+mlizbeth@users.noreply.github.com>
2020-12-01 15:12:53 -0600

    more graphical changes

--- src/application/controller/PhotonMainController.java
+++ src/application/controller/PhotonMainController.java
@@ -9,21 +9,31 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class PhotonMainController implements EventHandler<ActionEvent> {
 
 	@FXML
-	private Button saveButton, undoButton, redoButton, settingsButton; 
+	private Button saveButton, undoButton, redoButton, settingsButton, dropperTool, bucketTool, selectorTool, brushTool, eraserTool, stampTool; 
 	
-	private ImageView saveImage, undoImage, redoImage;
+	@FXML
+	private Canvas drawZone;
+		
+	@FXML
+	private ImageView drawImage;
+	
+	private ImageView saveImage, undoImage, redoImage, selectorToolImage, dropperToolImage, bucketToolImage, brushToolImage, eraserToolImage, stampToolImage;
 	
 	@Override
 	public void handle(ActionEvent event) {
-		// TODO Auto-generated method stub
+		System.out.println("TEST");
 		
 	}
 	
@@ -31,18 +41,29 @@ public class PhotonMainController implements EventHandler<ActionEvent> {
 	void initialize() {
 		
 		initalizeImages();
+		
+	
+		/*
+		 * TODO
+		 * Shapes/Tools/Stuff
+		 * Start with a blank canvas that you can draw on
+		 * Add layers to canvas
+		 * load image onto canvas
+		 * save image + canvas contents to 1 image file
+		 */
+		
+		/*
+		GraphicsContext gc = drawZone.getGraphicsContext2D();
+		gc.setFill(Color.GRAY);
+		gc.fillRect(0, 0, 750, 600);
+		*/
+		
+		
 			
 	}
 	
-	private void initalizeImages() {
-		
-		/*
-		 * old
-		 * saveImage = new Image(getClass().getResourceAsStream("/img/save.png"));
-		 * saveButton.setGraphic(new ImageView(saveImage));
-		 */
-		
-		saveImage = new ImageView(new Image(getClass().getResourceAsStream("/img/save.png")));
+	private void initalizeImages() {				
+		saveImage = new ImageView(new Image(getClass().getResourceAsStream("/img/save-white.png")));
 		saveImage.setFitHeight(25);
 		saveImage.setFitWidth(25);
 		saveButton.setGraphic(saveImage);
@@ -56,6 +77,38 @@ public class PhotonMainController implements EventHandler<ActionEvent> {
 		redoImage.setFitHeight(25);
 		redoImage.setFitWidth(25);
 		redoButton.setGraphic(redoImage);
+		
+		selectorToolImage = new ImageView(new Image(getClass().getResourceAsStream("/img/cursor-white.png")));
+		selectorToolImage.setFitHeight(30);
+		selectorToolImage.setFitWidth(30);
+		selectorTool.setGraphic(selectorToolImage);
+		
+		dropperToolImage = new ImageView(new Image(getClass().getResourceAsStream("/img/dropper-white.png")));
+		dropperToolImage.setFitHeight(30);
+		dropperToolImage.setFitWidth(30);
+		dropperTool.setGraphic(dropperToolImage);
+		
+		bucketToolImage = new ImageView(new Image(getClass().getResourceAsStream("/img/bucket-white.png")));
+		bucketToolImage.setFitHeight(30);
+		bucketToolImage.setFitWidth(30);
+		bucketTool.setGraphic(bucketToolImage);
+		
+		brushToolImage = new ImageView(new Image(getClass().getResourceAsStream("/img/brush-white.png")));
+		brushToolImage.setFitHeight(30);
+		brushToolImage.setFitWidth(30);
+		brushTool.setGraphic(brushToolImage);
+		
+		eraserToolImage = new ImageView(new Image(getClass().getResourceAsStream("/img/eraser-white.png")));
+		eraserToolImage.setFitHeight(30);
+		eraserToolImage.setFitWidth(30);
+		eraserTool.setGraphic(eraserToolImage);
+		
+		stampToolImage = new ImageView(new Image(getClass().getResourceAsStream("/img/stamp.png")));
+		stampToolImage.setFitHeight(30);
+		stampToolImage.setFitWidth(30);
+		stampTool.setGraphic(stampToolImage);
+		
+		selectorTool.setTooltip(new Tooltip("Selection tool lets you move objects on the canvas"));
 	}
 	
 	public void settingsButtonPushed(ActionEvent event) throws IOException {
